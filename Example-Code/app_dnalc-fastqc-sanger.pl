42b794fc4712a9b038c5da28671e0487c8d92c78
Matthew Vaughn <vaughn@tacc.utexas.edu>
2013-07-24 13:40:42 -0700

    Fixes to address warnings about empty values, etc

--- Example-Code/app_dnalc-fastqc-sanger.pl
+++ Example-Code/app_dnalc-fastqc-sanger.pl
@@ -5,6 +5,6 @@ use warnings;
 use iPlant::FoundationalAPI;
 
 my $api_instance = iPlant::FoundationalAPI->new();
-$api_instance->debug(1);
+$api_instance->debug(0);
 $api_instance->application_id('dnalc-fastqc-sanger-0.10.1');
 $api_instance->invoke();
